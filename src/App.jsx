c0c0701408782ce249230b2b4bf57a8dda79950d
idanberger22 <idanberger22@gmail.com>
2023-06-27 13:58:41 +0300

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -15,28 +15,33 @@ function App() {
   }
 
   return (<>
+    <div className='header'>Support</div>
 
-    <div className='header'>
-      Support
-    </div>
     <div className='center'>
       <form onSubmit={sendFeedback}>
+        <p>Choose a game :</p>
         <select name='game' required>
-          <option>captain coin</option>
-          <option>marble game</option>
+          <option>Captain coin</option>
+          <option>Marble game</option>
         </select>
-        <textarea name="message" placeholder="hhh" />
-        <button className='contact-button'> send mail</button>
+        <p>Please describe the issue :</p>
+        <textarea name="message" placeholder="I have a problem with..." required/>
+        <button className='contact-button'>send mail</button>
       </form>
       <div>
-        <p>For more urgent problems, you can use our live chat</p>
-        <button className='contact-button' onClick={() => window.gist.chat('open')}>open gist</button>
+        <div className='gist-wrapper'>
+          <p>For more urgent problems, you can use our live chat :</p>
+          <button className='contact-button' onClick={() => window.gist.chat('open')}>Live chat</button>
+        </div>
+
       </div>
     </div>
-
-    {popup && <div className='popup'>
-
-    </div>}
+    {popup && <> <div className='dark'/>
+    <div className='popup'>
+      <p>Thank you! We appreciate your feedback and do our best to improve.</p>
+      <button className='contact-button' onClick={()=>setPopup(false)}>Done</button>
+    </div>
+    </> }
 
     <div className='footer'>
       ®2023 Homeric entertainment
